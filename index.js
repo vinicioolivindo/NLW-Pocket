9e2bc1f86ef5f4402d4ea2c5791019264f8f122c
v1nny:) <vinicioolivindo92@gmail.com>
2024-09-12 09:46:59 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -39,7 +39,22 @@ const listarMetas = async () => {
         meta.checked = true
     })
     console.log('Meta(s) marcadas como concluída(s)');
+}
 
+const metasRealizadas = async () => {
+  const realizadas = metas.filter((meta) => {
+    return meta.checked
+  })
+  if(realizadas.length == 0){
+    console.log('Não existem metas realizadas :(');
+    return
+  }
+
+  await select({
+    message: "Metas Realizadas",
+    choices: [...realizadas]
+  })
+  
 }
 
 const start = async () => {
@@ -53,9 +68,13 @@ const start = async () => {
           value: "cadastrar",
         },
         {
-            name: "Listar metas",
-            value: "listar",
-          },
+          name: "Listar metas",
+          value: "listar",
+        },
+        {
+          name: "Metas realizadas",
+          value: "realizadas",
+        },
         {
           name: "Sair",
           value: "sair",
@@ -70,6 +89,9 @@ const start = async () => {
         break;
       case "listar":
         await listarMetas()
+        break;
+      case "realizadas":
+        await metasRealizadas()
         break;
       case "sair":
         console.log("Ate a proxima");
